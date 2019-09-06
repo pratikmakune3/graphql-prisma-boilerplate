78e06078ec5c7902428b1299d26b740987642b50
Pratik Makune <pratikmakune3@users.noreply.github.com>
2019-09-06 17:52:43 +0530

    Init Commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import { resolvers, fragmentReplacements } from './resolvers/index'
+import prisma from './prisma'
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context(request) {
+        return {
+            pubsub,
+            prisma,
+            request
+        }
+    },
+    fragmentReplacements
+})
+
+export { server as default }
